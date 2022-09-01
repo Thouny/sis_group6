eacdf1c01dfd6df57c11c3731d229119f7496f91
Danielfont <danielplafontaine@gmail.com>
2022-09-01 16:06:50 +1000

    Fix for twitter API 2
    Using tweepy

--- backend/scraper.py
+++ backend/scraper.py
@@ -3,23 +3,21 @@
 from http import client
 import tweepy
 
-consumer_key = "x" #Your API/Consumer key 
-consumer_secret = "x" #Your API/Consumer Secret Key
-access_token = "x"    #Your Access token key
-access_token_secret = "x" #Your Access token Secret key
+BEARER = "x"
+CONSUMER_KEY = "x"
+CONSUMER_SECRET = "x" 
+ACCESS_KEY = "x"    
+ACCESS_SECRET = "x" 
 
 #Pass in our twitter API authentication key
-client = tweepy.Client(
-    consumer_key, consumer_secret,
-    access_token, access_token_secret
-)
-
-search_query = "mc donalds"
-no_of_tweets =100
-
+client = tweepy.Client(bearer_token=BEARER,
+     consumer_key=CONSUMER_KEY, 
+     consumer_secret=CONSUMER_SECRET, 
+     access_token=ACCESS_KEY, 
+     access_token_secret=ACCESS_SECRET)
 
 try:
-    query = 'news'
+    query = 'mc donalds lang:en'
     tweets = client.search_recent_tweets(query=query, max_results=10)
     for tweet in tweets.data:
         print(tweet.text)
