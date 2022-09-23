49e7329c26e81be177a7f5072e307d0769875717
Anthony Vu <avu@palo-it.com>
2022-09-23 17:31:58 +1000

    Fix menu button not opening side bar

--- lib/presentation/root_page_tab_scaffold.dart
+++ lib/presentation/root_page_tab_scaffold.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:routemaster/routemaster.dart';
-import 'package:sis_group6/core/consts/home/home.dart';
+import 'package:sis_group6/controller/menu_controller.dart';
 import 'package:sis_group6/presentation/widgets/side_menu.dart';
 
 class RootPageTabScaffold extends StatefulWidget {
@@ -30,7 +31,7 @@ class _RootPageTabScaffoldState extends State<RootPageTabScaffold>
     final currentPath = Routemaster.of(context).currentRoute.fullPath;
 
     return Scaffold(
-      // appBar: _buildAppBar(context, currentPath),
+      key: context.read<MenuController>().scaffoldKey,
       drawer: const SideMenu(),
       body: HeroControllerScope(
         controller: MaterialApp.createMaterialHeroController(),
@@ -39,28 +40,6 @@ class _RootPageTabScaffoldState extends State<RootPageTabScaffold>
           stack: tabState.stacks[tabState.index],
         ),
       ),
-    );
-  }
-
-  AppBar _buildAppBar(BuildContext context, String currentPath) {
-    return AppBar(
-      backgroundColor: Colors.blue.shade900,
-      automaticallyImplyLeading: false,
-      elevation: 5.0,
-      leading: IconButton(
-        onPressed: () {},
-        icon: const Icon(Icons.menu),
-      ),
-      title: const Text("Dashboard"),
-      actions: [
-        Padding(
-          padding: const EdgeInsets.only(right: 16),
-          child: IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.settings),
-          ),
-        ),
-      ],
     );
   }
 }
