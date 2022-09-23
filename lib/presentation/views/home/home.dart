0bedb8b3effc00c3a720f69f0ba72778c9f4777a
Anthony Vu <avu@palo-it.com>
2022-09-23 14:26:39 +1000

    Improve Dashboard layout for responsiveness

--- lib/presentation/views/home/home.dart
+++ lib/presentation/views/home/home.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:sis_group6/bloc/tweets/tweets_bloc.dart';
-import 'package:sis_group6/core/theme/app.dart';
-import 'package:sis_group6/mock/sentiment_data.dart';
-import 'package:sis_group6/presentation/widgets/sentiment_chart.dart';
-import 'package:sis_group6/presentation/widgets/tweet_list_card.dart';
-import 'package:sis_group6/presentation/widgets/word_cloud.dart';
+import 'package:sis_group6/core/utils/responsive.dart';
+import 'package:sis_group6/presentation/views/home/dashboard.dart';
+import 'package:sis_group6/presentation/widgets/side_menu.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -14,71 +10,26 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final tweetsBloc = BlocProvider.of<TweetsBloc>(context);
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
+      body: SafeArea(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            _SearchBar(
-              key: const Key('$keyPrefix-SearchBar'),
-              onSubmitted: (value) {
-                if (value.isNotEmpty) {
-                  tweetsBloc.add(SearchTweetsEvent(value));
-                }
-              },
-            ),
-            Column(
-              children: [
-                SentimentChart(data: mockData),
-                const _TweetContent(key: Key('$keyPrefix-TweetsCard')),
-                const WordCloud(
-                  data: [],
-                ),
-              ],
+            // We want this side menu only for large screen
+            if (Responsive.isDesktop(context))
+              const Expanded(
+                // default flex = 1
+                // and it takes 1/6 part of the screen
+                child: SideMenu(),
+              ),
+            const Expanded(
+              // It takes 5/6 part of the screen
+              flex: 5,
+              child: DashboardView(),
             ),
           ],
         ),
       ),
     );
-  }
-}
-
-class _TweetContent extends StatelessWidget {
-  const _TweetContent({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(AppPaddingValues.xSmallHorizontalPadding),
-      height: AppHeightValues.sentimentChartHeight,
-      child: Card(
-        child: BlocBuilder<TweetsBloc, TweetsState>(
-          builder: (context, state) {
-            if (state is LoadedTweetsState) {
-              return TweetListCard(tweets: state.tweets);
-            } else if (state is LoadingTweetsState) {
-              const CircularProgressIndicator();
-            } else if (state is FailedTweetsState) {
-              return Text(state.message);
-            }
-            return const SizedBox.expand(
-              child: Center(child: Text('SEARCH A TOPIC')),
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class _SearchBar extends StatelessWidget {
-  const _SearchBar({Key? key, this.onSubmitted}) : super(key: key);
-
-  final Function(String value)? onSubmitted;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(onSubmitted: onSubmitted);
   }
 }
