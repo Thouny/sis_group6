f25023448ed2d24a16b7ec9f55ccc38bada0444e
Anthony Vu <avu@palo-it.com>
2022-09-29 11:56:50 +1000

    Create `SentimentService` and `SentimentRepository` to send API requests

--- lib/core/utils/injector.dart
+++ lib/core/utils/injector.dart
@@ -1,7 +1,13 @@
 import 'dart:async';
 
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:get_it/get_it.dart';
+import 'package:http/http.dart';
+import 'package:sis_group6/domain/repositories/sentiment.dart';
 import 'package:sis_group6/domain/repositories/twitter_repository.dart';
+import 'package:sis_group6/infrastructure/network/client/app.dart';
+import 'package:sis_group6/infrastructure/network/client/network.dart';
+import 'package:sis_group6/services/sentiment.dart';
 import 'package:sis_group6/services/twitter.dart';
 
 typedef AppRunner = void Function();
@@ -21,16 +27,24 @@ class Injector {
 }
 
 /// Register utils and misc tools.
-void _injectUtils() {}
+void _injectUtils() {
+  GetIt.I.registerLazySingleton<NetworkClient>(() {
+    return AppClient(Client(), ConnectivityService(Connectivity()));
+  });
+}
 
 void _injectDatabase() {}
 
 void _injectRepositories() {
+  GetIt.I.registerLazySingleton<SentimentRepository>(() {
+    return SentimentRepositoryImpl();
+  });
   GetIt.I.registerLazySingleton<TwitterRepository>(() {
     return TwitterRepositoryImpl();
   });
 }
 
 void _injectServices() {
+  GetIt.I.registerLazySingleton<SentimentService>(() => SentimentService());
   GetIt.I.registerLazySingleton<TwitterService>(() => TwitterService());
 }
