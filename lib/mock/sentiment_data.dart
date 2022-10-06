142e901bdc48b14297ce8b1aa4f5e83165e1b60d
Anthony Vu <avu@palo-it.com>
2022-10-06 23:26:32 +1100

    Remove unnecessary mock data

--- lib/mock/sentiment_data.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:sis_group6/presentation/models/sentiment_chart_data.dart';
-
-final mockData = [
-  const SentimentChartData('Positive', 0.7, label: '70%', color: Colors.green),
-  const SentimentChartData('Neutral', 0.2, label: '20%', color: Colors.orange),
-  const SentimentChartData('Negative', 0.1, label: '10%', color: Colors.red),
-];
