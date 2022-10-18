4f1452801ab70a955057f118f29e0f1699383bfd
Anthony Vu <avu@palo-it.com>
2022-10-18 17:10:15 +1100

    Remove unused code

--- lib/bloc/sentiment_over_time/sentiment_over_time_bloc.dart
+++ lib/bloc/sentiment_over_time/sentiment_over_time_bloc.dart
@@ -1,15 +1,9 @@
 import 'dart:async';
-import 'dart:math';
 
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
-import 'package:sis_group6/core/enums/sentiment.dart';
-import 'package:sis_group6/core/theme/keyword.dart';
-import 'package:sis_group6/domain/entities/keyword.dart';
-import 'package:sis_group6/domain/entities/tweet.dart';
 import 'package:sis_group6/domain/repositories/sentiment.dart';
-import 'package:sis_group6/presentation/models/keyword.dart';
 import 'package:sis_group6/presentation/models/sentiment_at_date_graph_data.dart';
 
 part 'sentiment_over_time_event.dart';
@@ -49,42 +43,5 @@ class SentimentOverTimeBloc
       print(err.toString());
       emit(FailedSentimentOverTimeState(err.toString()));
     }
-  }
-}
-
-extension _Helpers on SentimentOverTimeBloc {
-  // ignore: unused_element
-  List<KeywordModel> _generateKeywordModels(List<KeywordEntity> entities) {
-    final keywords = <KeywordModel>[];
-    for (var entity in entities) {
-      keywords.add(KeywordModel.fromEntity(
-        entity,
-        KeywordColors.random,
-        Random().nextBool(),
-      ));
-    }
-    return keywords;
-  }
-
-  // ignore: unused_element
-  int _getCount(List<TweetEntity> tweets, Sentiment sentiment) {
-    var count = 0;
-    for (final tweet in tweets) {
-      if (tweet.sentiment == sentiment) {
-        count++;
-      }
-    }
-    return count;
-  }
-
-  // ignore: unused_element
-  List<TweetEntity> _filterRetweets(List<TweetEntity> tweets) {
-    final filteredTweets = <TweetEntity>[];
-    for (final tweet in tweets) {
-      if (!tweet.text.contains('RT ')) {
-        filteredTweets.add(tweet);
-      }
-    }
-    return filteredTweets;
   }
 }
