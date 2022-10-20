4dfb9770f6233b7cd649c790a9ddd562ed6c3976
Anthony Vu <avu@palo-it.com>
2022-10-21 01:54:12 +1100

    Add `SelectSocialMediaWidget` to select/unselect social media

--- lib/core/enums/source.dart
+++ lib/core/enums/source.dart
@@ -4,6 +4,15 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 enum Source { twitter, reddit }
 
 extension Properties on Source {
+  String get label {
+    switch (this) {
+      case Source.twitter:
+        return 'Twitter';
+      case Source.reddit:
+        return 'Reddit';
+    }
+  }
+
   Icon get icon {
     switch (this) {
       case Source.twitter:
